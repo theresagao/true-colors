a244ba856185550969285a9f52eef6cd61ed5863
YOUR_NAME <YOUR_EMAIL>
2017-08-08 22:02:17 -0700

    first commit

---
+++ run.py
@@ -0,0 +1,47 @@
+print("Find out your true colors.")
+print("\n")
+
+print("Each letter refers to a set of three words. \nRead the words and describe which of the four letter choices is most like you.")
+print("\n")
+
+print("Rank the one that is most like you with a 4. Then rank the next three letter choices from 3-1 in descending preference.")
+print("\n")
+
+print("Row One")
+print("\n")
+
+print("A: Active, Opportunistic, Spontaneous")
+print("B: Parental, Traditional, Responsible")
+print("C: authentic, harmonious, compassionate")
+print("D: versatile, inventive, competent")
+
+a = input("Rank for A from 4 (most) - 1 (least):")
+b = input("Rank for B from 4 (most) - 1 (least):")
+c = input("Rank for C from 4 (most) - 1 (least):")
+d = input("Rank for D from 4 (most) - 1 (least):")
+
+print("Row Two")
+print("\n")
+
+print("E: Curious, Conceptual, Knowledgeable")
+print("F: Unique, Empathetic, Communicative")
+print("G: Practical, Sensible, Dependable")
+print("H: Competitive, Impetuous, Impactful")
+
+e = input("Rank for E from 4 (most) - 1 (least):")
+f = input("Rank for F from 4 (most) - 1 (least):")
+g = input("Rank for G from 4 (most) - 1 (least):")
+h = input("Rank for H from 4 (most) - 1 (least):")
+
+print("Row Three")
+print("\n")
+
+print("I: Loyal, Conservative, Organized")
+print("J: Devoted, Warm, Poetic")
+print("K: Realistic, Open-minded, Adventuresome")
+print("L: Theoretical, Seeking, Ingenious")
+
+i = input("Rank for I from 4 (most) - 1 (least):")
+j = input("Rank for J from 4 (most) - 1 (least):")
+k = input("Rank for K from 4 (most) - 1 (least):")
+l = input("Rank for L from 4 (most) - 1 (least):")
